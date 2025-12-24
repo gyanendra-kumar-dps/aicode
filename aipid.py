f616057fbb637aa10f9f0677b625502eea412f48
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2025-12-24 21:43:25 +0530

    first commit

--- aipid.py
+++ aipid.py
@@ -1,5 +1,5 @@
 import requests
-from config import api_key
+api_key='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'
 url = "https://router.huggingface.co/models/Salesforce/blip-image-captioning-base"
 headers={
     "Authorization":f"Bearer {api_key}"
