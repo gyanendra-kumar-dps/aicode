4cdd5d9a244cb19ba5e9b7d7f70d06b1059eb458
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2026-01-14 21:03:31 +0530

    first commit

---
+++ ttsapphw.py
@@ -0,0 +1,41 @@
+import pyttsx3
+import random
+json_to_write={}
+def setup_tts():
+    engine=pyttsx3.init()
+    engine.setProperty("rate", 150)
+    engine.setProperty("volume", 0.9)
+    return engine
+def speak(engine:pyttsx3.Engine,text):
+    if engine:
+        try:
+            engine.say(text)
+            engine.runAndWait()
+        except:
+            print(f"audio:{text}")
+    else:
+        print(f"audio:{text}")
+def get_samples():
+    sample=["Hello! I am your computer","Python is awesome","This is A.I speaking","Welcome to the future"]
+    return random.choice(sample)
+def main():
+    engine = setup_tts()
+    if engine:
+        print("type something")
+    else:
+        print("audio not audible")
+    speak(engine, "type something")
+    user_input_count=0
+    while True:
+        text = input("Type:").strip()
+        user_input_count+=1
+        if text.lower()=='exit':
+            speak(engine,"Goodbye")
+            break
+        elif text.lower()=='sample':
+            sample=get_samples()
+            speak(engine,sample)
+        else:
+            speak(engine,text)
+if __name__=='__main__':
+    main()
