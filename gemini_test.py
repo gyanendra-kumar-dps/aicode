572e90afd8d32683ea9500d0038bdc316f4efcb0
gyanendra-kumar-dps <gyanendra.kumar1210@gmail.com>
2026-01-26 21:45:08 +0530

    first commit

---
+++ gemini_test.py
@@ -0,0 +1,20 @@
+from google import genai
+from config import gemini_api_key
+client=genai.Client(api_key=gemini_api_key)
+def generate(prompt):
+    res=client.models.generate_content(
+        model='gemini-2.0-flash',
+        contents=prompt
+    )
+    return res.text
+def prompt(v_prompt,c_prompt,cc_prompt):
+    print(generate(v_prompt))
+    print(generate(c_prompt))
+    print(generate(cc_prompt))
+def main():
+    v_prompt=input("")
+    c_prompt=input("")
+    cc_prompt=input("")
+    print(prompt(v_prompt,c_prompt,cc_prompt))
+if __name__=='__main__':
+    main()
